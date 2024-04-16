e6efba6c91c1a66b0485d7183f411dfac563e950
LDROSSIII <89868818+LDROSSIII@users.noreply.github.com>
2024-04-16 13:05:44 -0400

    Update Insertion.sql
    The insertion should work now but I'll need to test this

--- Database Systems Gradebook/Insertion.sql
+++ Database Systems Gradebook/Insertion.sql
@@ -1,58 +1,44 @@
 INSERT INTO PROFESSOR
-    ([PROFESSOR_ID], [FNAME], [LNAME])
 VALUES
-    (1, Chunmei, Liu)
-GO
+    (1001, Chunmei, Liu);
 
 INSERT INTO COURSE
-    ([CRS_NUM], [DEPARTMENT], [CRS_NAME], [SEMESTER], [SCHOOL_YEAR], [PROFESSOR_ID])
 VALUES
-    (1, 'CEA', 'Database Systems', 'Spring', '2024', 1)
-GO
+    (432, 'CEA', 'Database Systems', 'Spring', '2024', (SELECT PROF_ID from PROFESSOR WHERE PROF_LNAME ='Liu'));
 
 INSERT INTO STUDENT
-    ([STUDENT_ID], [STUDENT_FNAME], [STUDENT_LNAME], [CRS_NUM])
 VALUES
-    (1, 'Logan', 'Ross'),
-    (2, 'Khloe', 'Wright'),
-    (3, 'Sara', 'Kanu')
-GO
+    (02998013, 'Logan', 'Ross'),
+    (02998014, 'Khloe', 'Wright'),
+    (02998015, 'Sara', 'Kanu');
 
 INSERT INTO ENROLLMENT
-    ([STUDENT_ID], [CRS_NUM])
 VALUES
-    (1,1),
-    (2,1),
-    (3,1)
-GO
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Ross'), (SELECT CRS_NUM from COURSE WHERE CRS_NAME = 'Database Systems')),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Wright'), (SELECT CRS_NUM from COURSE WHERE CRS_NAME = 'Database Systems')),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Kanu'), (SELECT CRS_NUM from COURSE WHERE CRS_NAME = 'Database Systems'));
 
 INSERT INTO CATEGORY
-    ([CATGORY_ID], [CATEGORY_TYPE], [CATEGORY_WEIGHT], [COURSE_NUM])
 VALUES
-    (1,'Assignments', 15, 1),
-    (2, 'Project and participation', 15, 1),
-    (3, 'Midterm exam', 30, 1),
-    (4, 'Final Exam', 40, 1)
-GO
+    (501,'Assignments', 15, (SELECT CRS_NUM from COURSE WHERE CRS_NAME = 'Database Systems')),
+    (502, 'Project and participation', 15, (SELECT CRS_NUM from COURSE WHERE CRS_NAME = 'Database Systems')),
+    (503, 'Midterm exam', 30, (SELECT CRS_NUM from COURSE WHERE CRS_NAME = 'Database Systems')),
+    (504, 'Final Exam', 40, (SELECT CRS_NUM from COURSE WHERE CRS_NAME = 'Database Systems'));
 
 INSERT INTO ASSIGNMENT
-    ([ASSIGNMENT_ID], [MAX_SCORE], [CATEGORY_ID])
 VALUES
-    (1, 50, 1),
-    (2, 100, 1),
-    (3, 100, 3)
-GO
+    (2001, 'Homework 1', 50, (SELECT CATEGORY_ID from CATEGORY WHERE CATEGORY_TYPE = 'Assignments')),
+    (2002, 'Homework 2', 100, (SELECT CATEGORY_ID from CATEGORY WHERE CATEGORY_TYPE = 'Assignments')),
+    (2003, 'Midterm' 100, (SELECT CATEGORY_ID from CATEGORY WHERE CATEGORY_TYPE = 'Midterm exam'));
 
 INSERT INTO GRADE
-    ([STUDENT_ID], [ASSIGNMENT_ID], [SCORE])
 VALUES
-    (1, 1, 45),
-    (1, 2, 93),
-    (1, 3, 84),
-    (2, 1, 50),
-    (2, 2, 97),
-    (2, 3, 95),
-    (3, 1, 49),
-    (3, 2, 92),
-    (3, 3, 98)
-GO
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Ross'), (SELECT ASSIGNMENT_ID from ASSIGNMENT WHERE ASSIGNMENT_NAME = 'Homework 1'), 45),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Ross'), (SELECT ASSIGNMENT_ID from ASSIGNMENT WHERE ASSIGNMENT_NAME = 'Homework 2'), 93),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Ross'), (SELECT ASSIGNMENT_ID from ASSIGNMENT WHERE ASSIGNMENT_NAME = 'Midterm'), 84),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Wright'), (SELECT ASSIGNMENT_ID from ASSIGNMENT WHERE ASSIGNMENT_NAME = 'Homework 1'), 50),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Wright'), (SELECT ASSIGNMENT_ID from ASSIGNMENT WHERE ASSIGNMENT_NAME = 'Homework 2'), 97),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Wright'), (SELECT ASSIGNMENT_ID from ASSIGNMENT WHERE ASSIGNMENT_NAME = 'Midterm'), 95),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Kanu'), (SELECT ASSIGNMENT_ID from ASSIGNMENT WHERE ASSIGNMENT_NAME = 'Homework 1'), 49),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Kanu'), (SELECT ASSIGNMENT_ID from ASSIGNMENT WHERE ASSIGNMENT_NAME = 'Homework 2'), 92),
+    ((SELECT STUDENT_ID from STUDENT WHERE STUDENT_LNAME ='Kanu'), (SELECT ASSIGNMENT_ID from ASSIGNMENT WHERE ASSIGNMENT_NAME = 'Midterm'), 98);
